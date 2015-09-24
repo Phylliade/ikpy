add437819990745d0f6f97a5dcc322c2d8cb1eec
buschbapti <baptiste.busch@inria.fr>
2015-09-24 14:25:40 +0200

    Add transformation library and base transformation for the model

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 
 setup(name='poppy_inverse_kinematics',
       version='1.0',
-      packages=['poppy_inverse_kinematics'],
+      packages=['poppy_inverse_kinematics','poppy_inverse_kinematics.tools'],
       package_dir={'': 'src'}
 )
--- src/poppy_inverse_kinematics/model.py
+++ src/poppy_inverse_kinematics/model.py
@@ -4,19 +4,29 @@ import math
 from . import forward_kinematics as fk
 from . import inverse_kinematic as ik
 from . import plot_utils as pl
+from .tools import transformations as tr
 
 class Model(object):
-    def __init__(self, links):
+    def __init__(self, links, rot=None, trans=None):
         self.links = links
         self.nb_joints = len(links)
         # initialize the parameters according to the kinematic library
         self.init_params(links)
+        # set the transformations from world to base
+        self.set_base_transformations(rot,trans)
         # initialize starting configuration
         self.current_joints = np.zeros(len(links))
         self.current_pose = self.forward_kinematic(self.current_joints)
 
+    def set_base_transformations(self, rot=None, trans=None):
+        if rot is None:
+            rot = np.eye(3)
+        if trans is None:
+            trans = [0,0,0]
+        self.world_to_base = tr.transformation(rot,trans)
+        self.base_to_world = tr.inverse_transform(self.world_to_base)
+
     def init_params(self, links):
-        # vectors = [([0,0,1],[1,0,0])]
         vectors = []
         for l in links:
             vectors.append(([0,0,1],[l,0,0]))
@@ -28,12 +38,18 @@ class Model(object):
     def forward_kinematic(self, q=None):
         if q is None:
             q = self.current_joints
+        # calculate the forward kinematic
         X = fk.get_nodes(self.parameters,q)
-        return X[0][-1]
+        # return the result in the world frame
+        W_X = tr.transform_point(X[0][-1],self.world_to_base)
+        return W_X
 
-    def inverse_kinematic(self, X, seed=None):
+    def inverse_kinematic(self, W_X, seed=None):
         if seed is None:
             seed = self.current_joints
+        # calculate the coordinate of the target in the robot frame
+        X = tr.transform_point(W_X,self.base_to_world)
+        # return the inverse kinematic
         return ik.inverse_kinematic(self.parameters,seed,X)
 
     def set_current_joints(self, q):
