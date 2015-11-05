d6b07854c978e5ede19bf0bfbbc6bdac63e53a45
Pierre Manceron <pierre.manceron@gmail.com>
2015-11-05 18:49:23 +0100

    Improve setup.py and Readme

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 
 # Installation
-To install the package, run :
+You have two options :
 
+
+1. From PyPI (recommended) : Simply run :
+```
+pip install poppy-inverse-kinematics
+```
+
+2. From source : First download and extract the archive, the run :
 ```
 python setup.py install
-```
+```    
+NB : You must have the proper rights to execute this command
 
-NB : You must have proper rights to execute this command
+
+# Dependencies and compatiblity
+The library can work with both versions of Python (2.7 and 3.x).
+It requires numpy and scipy.
+
+Sympy is optional and used for fast hybrid computation methods.
--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='poppy_inverse_kinematics',
-      version='1.0',
-      packages=['poppy_inverse_kinematics','poppy_inverse_kinematics.tools'],
-      package_dir={'': 'src'}
-)
+setup(name='poppy-inverse-kinematics',
+      version='1.0.2',
+      author="Pierre Manceron",
+      description="An inverse kinematics library aiming performance and modularity",
+      url="https://github.com/Phylliade/poppy-inverse-kinematics",
+      license="GNU GENERAL PUBLIC LICENSE Version 3",
+      packages=['poppy_inverse_kinematics', 'poppy_inverse_kinematics.tools'],
+      package_dir={'': 'src'},
+      install_requires=['numpy', 'scipy', 'sympy'],
+      classifiers=["Programming Language :: Python :: 2", "Programming Language :: Python :: 3", "Topic :: Scientific/Engineering"],
+      keywords='robotics inverse-kinematics'
+      )
