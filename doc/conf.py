6b5ec16e5ffa90122c6710fe797fedb26912ec55
Pierre Manceron <pierre.manceron@gmail.com>
2015-11-06 14:55:01 +0100

    Fix doc conf

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("../src/poppy_inverse_kinematics"))
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- General configuration ------------------------------------------------
 
