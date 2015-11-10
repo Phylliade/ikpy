247924085c7002b298df210d2dabc0684cc68184
Pierre Manceron <pierre.manceron@gmail.com>
2015-11-10 10:17:50 +0100

    Add setup_require to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 from setuptools import setup
 
 setup(name='poppy-inverse-kinematics',
-      version='1.0.2',
+      version='1.0.3',
       author="Pierre Manceron",
       description="An inverse kinematics library aiming performance and modularity",
       url="https://github.com/Phylliade/poppy-inverse-kinematics",
       license="GNU GENERAL PUBLIC LICENSE Version 3",
       packages=['poppy_inverse_kinematics', 'poppy_inverse_kinematics.tools'],
       package_dir={'': 'src'},
+      setup_requires=['numpy'],
       install_requires=['numpy', 'scipy', 'sympy'],
       classifiers=["Programming Language :: Python :: 2", "Programming Language :: Python :: 3", "Topic :: Scientific/Engineering"],
       keywords='robotics inverse-kinematics'
