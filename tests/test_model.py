2dfcfd1043075592bad2983567e6c72fc0be194b
Pierre Manceron <pierre.manceron@gmail.com>
2015-11-12 18:06:41 +0100

    Update tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,23 +3,24 @@ import numpy as np
 import poppy_inverse_kinematics.robot_utils
 import poppy_inverse_kinematics.model
 import poppy_inverse_kinematics.model_config
-import scripts_config
+import test_config
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
 
-        params = poppy_inverse_kinematics.model_config.from_urdf_file(scripts_config.urdf_file, scripts_config.base_link, scripts_config.last_link_vector)
-        self.robot = poppy_inverse_kinematics.model.Model(params, computation_method="hybrid", simplify=False)
-
+        params = poppy_inverse_kinematics.model_config.from_urdf_file(test_config.urdf_file, test_config.base_link, test_config.last_link_vector)
+        self.robots = []
+        methods = ["default", "symbolic", "hybrid"]
         target = [-0.1, 0.1, 0.1]
-
-        self.robot.target = target
+        for index, method in enumerate(methods):
+            self.robots.append(poppy_inverse_kinematics.model.Model(params, computation_method=method, simplify=False))
+            self.robots[index].target = target
 
     def test_ik(self):
-        self.robot.goto_target()
-        np.testing.assert_almost_equal(self.robot.forward_kinematic(), self.robot.target, decimal=3)
-
+        for robot in self.robots:
+            robot.goto_target()
+            np.testing.assert_almost_equal(robot.forward_kinematic(), robot.target, decimal=3)
 
 if __name__ == '__main__':
     unittest.main()
