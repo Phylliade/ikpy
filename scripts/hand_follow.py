607ac16c1742234d139810706da770b9f28faac8
Pierre Manceron <pierre.manceron@gmail.com>
2015-11-23 17:59:41 +0100

    Improve hand_follow

--- scripts/hand_follow.py
+++ scripts/hand_follow.py
@@ -1,16 +1,27 @@
 import poppy_inverse_kinematics.creature as model_creature
 import numpy as np
 import poppy_inverse_kinematics.meta_creature as meta_creature
+import time
 
 # parameters
 activate_follow = True
+interface_type = "robot"
+plot = False
 
 # Create creatures
 right_arm = model_creature.creature("torso_right_arm")
 left_arm = model_creature.creature("torso_left_arm")
-torso = meta_creature.MetaCreature(interface_type="vrep", creature_type="torso")
+torso = meta_creature.MetaCreature(interface_type=interface_type, creature_type="torso")
 torso.add_model(right_arm)
 torso.add_model(left_arm)
+
+# Init pypot robot
+if interface_type == "robot":
+    for m in torso.pypot_object.motors:
+        m.compliant = False
+        m.goal_position = 0
+    time.sleep(10)
+
 
 # Set right arm position
 target_right = np.array([-0.27, -0.2, 0.5])
@@ -25,4 +36,8 @@ if activate_follow:
     left_arm.goto_target()
 
 # Plot result
-torso.plot_meta_model()
+if plot:
+    torso.plot_meta_model()
+
+if interface_type == "robot":
+    torso.pypot_object.close()
