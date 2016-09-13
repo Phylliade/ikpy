2c38cb575cef19a24c7a412c420924feddee7e35
Pierre Manceron <pierre.manceron@gmail.com>
2016-09-13 18:26:42 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='ikpy',
-      version='2.2.3',
+      version='2.2.4~dev',
       author="Pierre Manceron",
       description="An inverse kinematics library aiming performance and modularity",
       url="https://github.com/Phylliade/ikpy",
