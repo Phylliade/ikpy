daaf1db7f31dcf71b45f5039eac63acd278c2751
Loïc Dauphin <astralien3000@yahoo.fr>
2016-09-19 12:52:58 +0200

    use of relative import in blender_utils.py

--- src/ikpy/blender_utils.py
+++ src/ikpy/blender_utils.py
@@ -28,7 +28,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from ikpy import matrix_link
+from . import matrix_link
 import sympy
 import json
 import subprocess
