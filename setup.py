69cf5c16742837b1eaee41c08b6fe02c0d8acb74
Pierre Manceron <pierre.manceron@linux.com>
2019-03-17 17:07:37 +0100

    Python2 fix

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_version():
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 
