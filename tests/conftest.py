b165a65d615daa8a21cfde0217b989fbb1b9d413
Pierre Manceron <pierre.manceron@linux.com>
2020-04-26 19:21:14 +0200

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,15 +28,7 @@ def baxter_urdf():
 
 @pytest.fixture
 def baxter_left_arm():
-    with open("../resources/baxter/baxter_left_arm.json", "r") as fd:
-        baxter_left_arm_elements = json.load(fd)
-
-    baxter_left_arm_chain = Chain.from_urdf_file(
-        "../resources/baxter/baxter.urdf",
-        base_elements=baxter_left_arm_elements,
-        last_link_vector=[0, 0.18, 0],
-        active_links_mask=3 * [False] + 10 * [True],
-        symbolic=False)
+    baxter_left_arm_chain = Chain.from_json_file("../resources/baxter/baxter_left_arm.json")
     return baxter_left_arm_chain
 
 
